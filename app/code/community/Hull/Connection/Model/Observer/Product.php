74182b4e769ce140a17649b5a750da27e3e3e834
Xavier Cambar <xcambar@gmail.com>
2013-09-13 16:54:14 +0200

    Updates product entities on Hull after product save in Magento

--- app/code/community/Hull/Connection/Model/Observer/Product.php
+++ app/code/community/Hull/Connection/Model/Observer/Product.php
@@ -1,8 +1,22 @@
 <?
 class Hull_Connection_Model_Observer_Product
 {
-  public function updateEntity()
+  public function updateEntity($observer)
   {
+    $event = $observer->getEvent();
+    $product = $event->getProduct();
+    $productUrl = $product->getProductUrl();
+    $client = $this->getClient();
+    try {
+      $res = $client->put('entity', array('uid' => $productUrl));
+    } catch (Exception $e) {
+      Mage::log($e->getMessage());
+    }
+  }
+
+  public function getClient()
+  {
+    return Mage::helper('hull_connection')->getClient();
   }
 }
 
